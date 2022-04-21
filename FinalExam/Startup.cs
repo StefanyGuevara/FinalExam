eead69c86551023b791888f38934d82afcc9cfe1
StefanyGuevara <stefany.m.guevara00@gmail.com>
2022-04-20 22:12:10 -0600

    changes

--- FinalExam/Startup.cs
+++ FinalExam/Startup.cs
@@ -1,15 +1,12 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Bowler.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace FinalExam
+namespace Mission13
 {
     public class Startup
     {
@@ -23,7 +20,12 @@ namespace FinalExam
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<QuotesDbContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:QuotesDbConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,10 +37,11 @@ namespace FinalExam
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -48,9 +51,7 @@ namespace FinalExam
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
