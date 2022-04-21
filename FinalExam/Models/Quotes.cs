3d43f1d1e8156a2768218b77358881b09a25f12b
StefanyGuevara <stefany.m.guevara00@gmail.com>
2022-04-20 21:52:21 -0600

    Add project files.

---
+++ FinalExam/Models/Quotes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quote.Models
+{
+   
+    public class Quotes
+    {
+        [Key]
+        [Required]
+        public int QuotesID { get; set; }
+
+        public string Author { get; set; }
+
+        public int Date { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Citation { get; set; }
+
+    }
+}
---
+++ FinalExam/Models/QuotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bowler.Models
+{
+    public class QuotesDbContext: DbContext
+    {
+        public QuotesDbContext(DbContextOptions<QuotesDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Quote> Bowlers { get; set; }
+    }
+}
